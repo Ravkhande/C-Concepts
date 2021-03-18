9182bf829def7914aba0de5451b2665b35ce5a3b
Pratik Pralhad Ravkhande <62715180+Ravkhande@users.noreply.github.com>
2021-03-18 11:53:21 +0530

    Add files via upload

---
+++ Function/swap3.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+/*
+This program gives error because c++ follow top to bottom approach and the error is arises due to we don't declared the swap function before the main
+to solve this error we have to declare it */
+void swap(int a, int b);
+
+int main()
+{
+	int a = 0, b = 0, iResult = 0;
+	cout << "Enter the two numbers:" << endl;
+	cin >> a;
+	cin >> b;
+
+	cout << "number before swapping :" << a << "\t" << b << "\n";
+
+	swap(a, b);
+
+	return 0;
+
+}
+
+void swap(int a, int b)
+{
+	a = a + b;
+	b = a - b;
+	a = a - b;
+
+	cout << "number after swapping : " << a << "\t" << b << "\n";
+
+}
